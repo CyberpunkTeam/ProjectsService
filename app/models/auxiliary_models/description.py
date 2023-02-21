9c28cd09d3c48e1ae823e3c188947cfc43253288
matifonseca <matias.fonseca@mercadolibre.com>
2023-02-21 20:10:57 -0300

    change file attached to dict

--- app/models/auxiliary_models/description.py
+++ app/models/auxiliary_models/description.py
@@ -4,7 +4,7 @@ from app.models.custom_base_model import CustomBaseModel
 
 
 class Description(CustomBaseModel):
-    files_attached: Optional[List[str]] = []
+    files_attached: Optional[dict] = []
     functional_requirements: Optional[List[str]] = []
     non_function_requirements: Optional[List[str]] = []
     summary: Optional[str] = ""
@@ -12,7 +12,7 @@ class Description(CustomBaseModel):
     @staticmethod
     def get_schema():
         return {
-            "files_attached": list,
+            "files_attached": dict,
             "functional_requirements": list,
             "non_function_requirements": list,
             "summary": str,
